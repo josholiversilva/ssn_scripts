704a581ceee413b74897f4de2b82cd08342b45f5
Josh Silva <josholiversilva@gmail.com>
2023-11-16 01:06:52 -0800

    Add email

--- pairings.py
+++ pairings.py
@@ -1,6 +1,9 @@
 import random
+import smtplib
+from email.message import EmailMessage
 
 PAIRINGS_LIST = "pairings_list.txt"
+FROM_EMAIL = "secretsantaneyigas@gmail.com"
 
 def get_names_and_emails():
     pairings_list = open(PAIRINGS_LIST, "r")
@@ -15,22 +18,20 @@ def get_names_and_emails():
     return pairings
 
 def create_pairings(names_and_emails):
-    # secret santa to receiver
+    # receiver to secret santa
     pairings = {}
     receivers = set()
     previous_secret_santas = get_previous_secret_santas()
 
     for name_and_email in names_and_emails:
-        print(receivers)
         name, email = name_and_email.split(",")
         receiver_name_and_email = choose_pairing(name_and_email, names_and_emails, previous_secret_santas, receivers)
         pairings[name_and_email] = receiver_name_and_email
 
-    print(pairings)
     return pairings
 
 def choose_pairing(ssn_name_and_email, receiver_names_and_emails, previous_secret_santas, receivers):
-    ssn_name, ssn_email = ssn_name_and_email.split(",")
+    ssn_name, ssn_email = ssn_name_and_email.split(",") 
     receiver_name_and_email = random.choice(receiver_names_and_emails)
     receiver_name, receiver_email = receiver_name_and_email.split(",")
 
@@ -40,6 +41,24 @@ def choose_pairing(ssn_name_and_email, receiver_names_and_emails, previous_secre
 
     receivers.add(receiver_name)
     return receiver_name_and_email
+
+def send_email(pairings):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(FROM_EMAIL, "udsn orou ryhf zcmk")
+
+    for ssn,receiver in pairings.items():
+        ssn_name,ssn_email = ssn.split(",")
+        receiver_name,_ = receiver.split(",")
+
+        msg = EmailMessage()
+        msg['Subject'] = "Secret Santa Pairings!!! <3"
+        msg['From'] = FROM_EMAIL
+        msg['To'] = ssn_email
+        msg.set_content("Hi {}, you have: {}".format(ssn_name, receiver_name))
+        s.send_message(msg)
+
+    s.quit()
 
 def get_previous_secret_santas():
     # change this to read from pairings.json as years go by
@@ -59,5 +78,6 @@ def get_previous_secret_santas():
 
 if __name__ == '__main__':
     names_and_emails = get_names_and_emails()
-    create_pairings(names_and_emails)
+    pairings = create_pairings(names_and_emails)
+    send_email(pairings)
 
